1d14e8854fa8fbe8c30104f21321875dfb7807ae
Paul Simroth <paul.simroth@proton.me>
2023-03-21 14:49:08 +0100

    formatting code

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import styles from "../styles";
-import { slideIn, staggerContainer, textVariant} from "../utils/motion";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
@@ -10,7 +10,7 @@ const Hero = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{once: false, amount: 0.25}}
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
@@ -30,22 +30,22 @@ const Hero = () => (
         variants={slideIn("right", "tween", 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]"/>
-          <img 
-            src="/cover.png" 
-            alt="cover" 
-            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
-          />
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+        <img
+          src="/cover.png"
+          alt="cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
 
-          <a href="#explore">
-            <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
-              <img 
-                src="/stamp.png"
-                alt="stamp"
-                className="sm:w[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
-              />
-            </div>
-          </a>
+        <a href="#explore">
+          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+            <img
+              src="/stamp.png"
+              alt="stamp"
+              className="sm:w[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+            />
+          </div>
+        </a>
       </motion.div>
     </motion.div>
   </section>
--- sections/World.jsx
+++ sections/World.jsx
@@ -7,43 +7,43 @@ import { staggerContainer, fadeIn } from "../utils/motion";
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
-  <motion.div
-    variants={staggerContainer}
-    initial="hidden"
-    whileInView="show"
-    viewport={{once: false, amount: 0.25}}
-    className={`${styles.innerWidth} mx-auto flex flex-col`}
-  >
-    <TypingText title="| People of Metaversus"/>
-    <TitleText title={<>
-      Search friends around you and invite them to play together in the same world!
-    </>}/>
     <motion.div
-      variant={fadeIn("up", "tween", 0.3, 1)}
-      className="relative mt-[68px] flex w-full"
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <img 
-        src="/map.png"
-        alt="map"
-        className="w-full h-full object-cover"
-      />
-      <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-        <img 
-          src="people-01.png" alt="people" className="w-full h-full"
+      <TypingText title="| People of Metaversus" />
+      <TitleText title={<>
+        Search friends around you and invite them to play together in the same world!
+      </>} />
+      <motion.div
+        variant={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex w-full"
+      >
+        <img
+          src="/map.png"
+          alt="map"
+          className="w-full h-full object-cover"
         />
-      </div>
-      <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-        <img 
-          src="people-02.png" alt="people" className="w-full h-full"
-        />
-      </div>
-      <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-        <img 
-          src="people-03.png" alt="people" className="w-full h-full"
-        />
-      </div>
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="people-01.png" alt="people" className="w-full h-full"
+          />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="people-02.png" alt="people" className="w-full h-full"
+          />
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="people-03.png" alt="people" className="w-full h-full"
+          />
+        </div>
+      </motion.div>
     </motion.div>
-  </motion.div>
   </section>
 );
 
